2814f9ff08404613088656c09de6f02805d67b04
Keith Tweed <ktweed@ualberta.ca>
2023-07-02 19:23:11 -0600

    Add function stubs for get_tnc and set_tnc, as well as a restore_tnc method to get the parameters from flash.

--- TNC/src/main.cpp
+++ TNC/src/main.cpp
@@ -20,6 +20,51 @@ AFSKClient audio(&radio, PB8);
 AX25Client ax25(&audio);
 APRSClient aprs(&ax25);
 
+struct TNCparams {
+  String callsign;
+  int ssid;
+  int path;
+  String tocall;
+  int dwait;
+  int persist;
+  String btext;
+  int slot_time;
+  int txd;
+  int dupe_time;
+} jasper;
+
+void restore_tnc() {
+  // TODO: Pull the following parameters from flash
+  jasper.callsign = "VA6MPL";
+  jasper.ssid = 11;
+  jasper.path = 1;
+  jasper.tocall = "APZ0BX";
+  jasper.dwait = 0;
+  jasper.persist = 63;
+  jasper.btext = "Hello World";
+  jasper.slot_time = 15;
+  jasper.txd = 30;
+  jasper.dupe_time = 30;
+}
+
+int set_tnc(String callsign_jp, int ssid_jp, int path_jp, String tocall_jp, int dwait_jp, int persist_jp, String btext_jp, int slot_time_jp, int txdelay_jp, int dupe_time_jp) {
+  jasper.callsign = callsign_jp;
+  jasper.ssid = ssid_jp;
+  jasper.path = path_jp;
+  jasper.tocall = tocall_jp;
+  jasper.dwait = dwait_jp;
+  jasper.persist = persist_jp;
+  jasper.btext = btext_jp;
+  jasper.slot_time = slot_time_jp;
+  jasper.txd = txdelay_jp;
+  jasper.dupe_time = dupe_time_jp;
+  // TODO: Write the following parameters to flash
+}
+
+TNCparams get_tnc() {
+  return jasper;
+}
+
 void setup() {
 
   Serial.begin(9600);
@@ -40,7 +85,7 @@ void setup() {
     while(true);
   }
 
-  state = ax25.begin("VA6MPL", 11, 48);
+  state = ax25.begin((char*)jasper.callsign.c_str(), jasper.ssid, 48);
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println("AX25 initialized");
   } else {
@@ -64,7 +109,7 @@ void loop() {
   Serial.print(F("Sending APRS packet with position DO33fl at millis: "));
   Serial.println(millis());
 
-  int state = aprs.sendPosition("APL0BX", 0, "5328.7N", "11332.52W", "Test packet");
+  int state = aprs.sendPosition((char*)jasper.tocall.c_str(), jasper.path, "5328.7N", "11332.52W", (char*)jasper.btext.c_str());
 
   Serial.print("Packet sent at millis: ");
   Serial.println(millis());
@@ -88,4 +133,6 @@ void loop() {
     Serial.print(F("failed, code "));
     Serial.println(state);
   }
+  
+  delay(10000);
 }
